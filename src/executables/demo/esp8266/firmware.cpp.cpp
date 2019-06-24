4a3bdf907dca04eba6b7e76d9d9f471767caaf1c
JKolokoltsev <j.kolokoltsev@gmail.com>
2019-06-24 18:20:16 -0500

    Removed esp8266 (it's a separate template project now)

--- src/executables/demo/esp8266/firmware.cpp.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by morrigan on 4/12/19.
-//
-
-int main(){
-    return 0;
-}
-
--- src/executables/tutorial/00_staticplot.cpp
+++ src/executables/tutorial/00_staticplot.cpp
@@ -9,6 +9,24 @@
 
 using namespace std;
 
+/*
+ * In the DistPipelineFWK gnuplot is used as one of the main plotters. From C++ programs
+ * it can be executed in two different ways:
+ *
+ * 1. As a console command
+ * 2. As a child process
+ *
+ * Here we consider the first case, it is the most straightforward and the easiest one.
+ * It consists of two steps:
+ *
+ * 1.1. Prepare gnuplot script ASCII text in the string buffer
+ * 2.1. Send this buffer throug a pipe to the gnuplot input
+ *
+ * This approach has no limits on the complexity of the graphic to be plotted, however it is static
+ * and slower than the second one. Another disadvantage is the requirement to prepare plotting string
+ * completely and explicitly by a hand.
+ */
+
 int main(int argc, char** argv){
 
     //this command will be executed in the terminal and has the next construction:
